6a206ef09efd0a3edab209a424ef29b00a73e356
mpwwww <111195758+mpwwww@users.noreply.github.com>
2023-09-24 07:05:42 -0400

    recover dark mode!

--- public/script.js
+++ public/script.js
@@ -3,21 +3,32 @@ const darkModeToggle = document.querySelector("#dark-mode");
 const enableDarkMode = () => {
   document.body.classList.add('darkmode');
   localStorage.setItem('darkMode', 'enabled');
-}
+} 
+if (darkMode === 'enabled') {
+    enableDarkMode();
+  }
+const enableBrightMode = () => {
+    document.body.classList.add('brightmode');
+
+} 
+
+
 const disableDarkMode = () => {
   document.body.classList.remove('darkmode');
   localStorage.setItem('darkMode', null);
+//   enable bright mode
 }
-if (darkMode === 'enabled') {
-  enableDarkMode();
-}
+
 darkModeToggle.addEventListener('click', () => {
 console.log("click");
-  darkMode = localStorage.getItem('darkMode'); 
-  
+console.log(darkMode);
+darkMode = localStorage.getItem('darkMode'); 
+
   if (darkMode !== 'enabled') {
+    document.body.classList.remove('brightmode')
     enableDarkMode();
   } else {  
-    disableDarkMode(); 
-  }
+    console.log("dark mode disabled");
+    enableBrightMode();
+    disableDarkMode();  }
 });
