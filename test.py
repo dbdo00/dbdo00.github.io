e4c640e27f41a3df79825703e1198a694de557df
mpwwww <111195758+mpwwww@users.noreply.github.com>
2023-12-03 04:40:28 -0500

    update

--- test.py
+++ test.py
@@ -9,13 +9,13 @@ print(text)
 
 img = blog.linked_images(text)
 print(img)
-f = lambda x: os.path.normpath(x)
+path = lambda x: os.path.normpath(x)
 img = [ 
-    "markdown/" + f(x) for x in img 
+    "markdown/" + path(x) for x in img 
 ]
 
 print(img)
 
 blog.publish_images(img, public_dir=post_dir)
 
-blog.main() 
+# blog.main() 
