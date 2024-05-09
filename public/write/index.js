9b71c6502fdbdb67df094bcfcdb8b6a776ad89f2
mpwwww <111195758+mpwwww@users.noreply.github.com>
2024-05-09 15:37:36 -0400

    auto login

--- public/write/index.js
+++ public/write/index.js
@@ -3,40 +3,26 @@ const outputToken = document.getElementById("output-token");
 const outputEmail = document.getElementById("output-email");
 
 
-// try to log in, if fail, then use the netlify authenticator to login
-
-const authenticator = new netlify.default({})
-let token;
-
-
-authenticator.authenticate(
-    // Set the OAuth provider and token scope
-    // Provider can be "github", "gitlab", or "bitbucket"
-    // The scopes available depend on your OAuth provider
-    { provider: "github", scope: "repo user"  },
-    async function (error, data) {
-        if (error) {
-            console.error(error);
-        } else {
-            token = await data.token
-           
-        }
-    }
-);
-
-try {
-    login(token);
-} catch (error) {
-    console.error(error);
-} 
-
-
-
 anchorTag.addEventListener("click", (event) => {
     event.preventDefault();
-    // netlify.configure({site_id: "94385d69-9efa-4e94-9d52-5c34b1afc240"});
-    login(token);
 
+    const authenticator = new netlify.default({})
+    // netlify.configure({site_id: "94385d69-9efa-4e94-9d52-5c34b1afc240"});
+
+    authenticator.authenticate(
+        // Set the OAuth provider and token scope
+        // Provider can be "github", "gitlab", or "bitbucket"
+        // The scopes available depend on your OAuth provider
+        { provider: "github", scope: "repo user"  },
+        async function (error, data) {
+            if (error) {
+                console.error(error);
+            } else {
+                const token = data.token;
+                login(token);   
+            }
+        }
+    );
 });
 
 
