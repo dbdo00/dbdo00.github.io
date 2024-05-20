3eaa3db937c4f8e2376d2b99d5f2b10e7bf1b692
dbdo <dbdo>
2024-05-20 10:14:49 -0400

    function in the pandoc filter

--- .pandoc/filter.lua
+++ .pandoc/filter.lua
@@ -1,6 +1,19 @@
+function remove_class_from_element(elem, class_to_remove)
+	if elem.classes then
+		for i, class in ipairs(elem.classes) do
+			if class == class_to_remove then
+				table.remove(elem.classes, i)
+				break
+			end
+		end
+	end
+	return elem
+end
+
 function add_rss_icon(el)
   if el.classes:includes("hasrss") then
     -- Create an img element
+    remove_class_from_element(el, "hasrss")
     local img = pandoc.RawInline('html', '<img src="syndicated-feed-icon.gif"> </img>')
     
     -- Append the img element after the current element
