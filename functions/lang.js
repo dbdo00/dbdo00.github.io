a4b3c29c26ba9b40e18215d624aca5d180ab1db6
dbdo <dbdo>
2024-07-25 16:44:11 -0400

    set up things for language detection. 	modified:   .pandoc/filter.lua 	modified:   functions/api.ts 	new file:   functions/lang.js 	new file:   index_zh.md 	modified:   makefile 	modified:   netlify.toml 	renamed:    public/index.html -> public/static/index.html 	new file:   public/static/index_zh.html 	new file:   public/static/syndicated-feed-icon.gif

---
+++ functions/lang.js
@@ -0,0 +1,39 @@
+import fs  from 'node:fs';
+
+const index =  fs.readFileSync('public/static/index.html', 'utf8', (err, data) => {
+	if (err){
+		console.error(err);
+	}
+    return data 
+}) 
+
+const index_zh =  fs.readFileSync('public/static/index_zh.html', 'utf8', (err, data) => {
+	if (err){
+		console.error(err);
+	}
+    return data 
+}) 
+
+export default async (request, context) => {
+	console.log("the accept-language header is: " + request.headers.get("accept-language"));
+	var lang = request.headers.get("accept-language");
+	if (!lang){
+		return Response.json({
+			statusCode: 200,
+			statusText: "language unknown\n"})
+	}
+	if (lang.split(",")[0].substring(0,2) === "en" ){
+		const myHeaders = new Headers(); // Currently empty
+		myHeaders.append("Content-Type", "text/html; charset=utf-8");
+		const myOptions = { status: 200, statusText: "SuperSmashingGreat!", 
+					headers: myHeaders
+					};
+
+		return new Response(index, myOptions)
+	}
+	else {
+	return Response.json({
+		statusCode:200
+		})
+	}
+};
