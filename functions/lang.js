a7211d55717dfced0a77ba33ce39d6c45aba6ff1
dbdo <dbdo>
2024-07-25 19:02:24 -0400

    enable zh language index page

--- functions/lang.js
+++ functions/lang.js
@@ -7,12 +7,22 @@ const index =  fs.readFileSync('public/static/index.html', 'utf8', (err, data) =
     return data 
 }) 
 
+
+const myHeaders = new Headers();
+myHeaders.append("Content-Type", "text/html; charset=utf-8");
+const myOptions = { status: 200, 
+	statusText: "SuperSmashingGreat!", 
+	headers: myHeaders
+};
+
+
 const index_zh =  fs.readFileSync('public/static/index_zh.html', 'utf8', (err, data) => {
 	if (err){
 		console.error(err);
 	}
     return data 
 }) 
+
 
 export default async (request, context) => {
 	console.log("the accept-language header is: " + request.headers.get("accept-language"));
@@ -23,13 +33,10 @@ export default async (request, context) => {
 			statusText: "language unknown\n"})
 	}
 	if (lang.split(",")[0].substring(0,2) === "en" ){
-		const myHeaders = new Headers(); // Currently empty
-		myHeaders.append("Content-Type", "text/html; charset=utf-8");
-		const myOptions = { status: 200, statusText: "SuperSmashingGreat!", 
-					headers: myHeaders
-					};
-
 		return new Response(index, myOptions)
+	}
+	else if (lang.split(",")[0].substring(0,2) === "zh"){
+		return new Response(index_zh, myOptions)
 	}
 	else {
 	return Response.json({
